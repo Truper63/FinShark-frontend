a6968a51b26cab8a4a54cb8121cfd8bc48fe7011
Shawn Rupert <Truper63@users.noreply.github.com>
2025-12-27 22:48:46 -0500

    Video 4 Updates

--- src/Components/CardList/CardList.tsx
+++ src/Components/CardList/CardList.tsx
@@ -1,16 +1,16 @@
-import React from 'react'
-import Card from '../Card/Card'
+import React, { JSX } from "react";
+import Card from "../Card/Card";
 
 interface Props {}
 
-const CardList = (props: Props) => {
+const CardList: React.FC<Props> = (props: Props): JSX.Element => {
   return (
     <div>
-        <Card companyName='Apple, Inc.' ticker='AAPL' price={100}/>
-        <Card companyName='Microsoft' ticker='MSFT' price={200}/>
-        <Card companyName='Tesla' ticker='TSLA' price={300}/>
+      <Card companyName="Apple, Inc." ticker="AAPL" price={100} />
+      <Card companyName="Microsoft" ticker="MSFT" price={200} />
+      <Card companyName="Tesla" ticker="TSLA" price={300} />
     </div>
-  )
-}
+  );
+};
 
-export default CardList
+export default CardList;
